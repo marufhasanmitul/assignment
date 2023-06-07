5baea822bfe4464f3a4c4dd3acee7e3390209cb0
Maruf Hasan <marufhasanmitul@gmail.com>
2023-06-07 06:01:15 +0600

    btn,center

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,113 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
+void main(){
+  runApp(MyApp() );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+   return MaterialApp(
+     debugShowCheckedModeBanner: false,
+     home: HomeScreen(),
+   );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        title: Text('Basic Widget'),
+        centerTitle: true,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: (){},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      body:
+      Center(
+        child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             //Elevated Button
+             ElevatedButton(
+               onPressed: () {},
+               child: Text('Btn'),
+               style: ElevatedButton.styleFrom(
+                 backgroundColor: Colors.red,
+                 //minimumSize: Size(100, 30),
+                 elevation: 5,
+                 padding: EdgeInsets.symmetric(horizontal: 100,vertical: 40),
+                 shadowColor: Colors.purple,
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(12),
+                   side: BorderSide(
+                     color: Colors.purple,
+                     width: 3
+                   )
+                 )
+
+
+               ),
+
+
+              ),
+             //Text Button
+             TextButton(
+
+                 onPressed: (){},
+                 child: Text('Text Button',style: TextStyle(color: Colors.white),),
+                 style:TextButton.styleFrom(
+                   backgroundColor: Colors.green,
+                   elevation: 5,
+                   padding: EdgeInsets.symmetric(horizontal: 100,vertical: 10)
+                 ) ,
+             ),
+             //outLine Button
+             OutlinedButton(
+                 onPressed: (){},
+                 child: Text('OuteLine Button'),
+                 style: OutlinedButton.styleFrom(
+                   side: BorderSide(
+                     color: Colors.blue
+                   )
+                 ),
+             ),
+             //Outline Btn with Icon
+             OutlinedButton.icon(
+                 onPressed: (){
+                   },
+                 icon: Icon(Icons.account_box_outlined),
+                 label: Text('Outline Btn with Icon'),
+                 style:OutlinedButton.styleFrom(
+                   side: BorderSide(color: Colors.red)
+                 ) ,
+
+             ),
+             //Icon Btn
+             IconButton(
+                 onPressed: (){},
+                 icon:Icon(Icons.add)
+             ),
+             
+             Image.asset('Images/b.jpg')
+
+
+           ],
+         ),
+      ),
+      
+
+
     );
   }
 }
+
