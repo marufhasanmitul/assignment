382df9b38e24531efd15a1f31577bafee3f02d87
Maruf Hasan <marufhasanmitul@gmail.com>
2023-06-08 07:12:36 +0600

    Tab_bar,Fragment,Tab_Bar,DefaultTabController

---
+++ lib/Fragment/Social.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class Social extends StatelessWidget{
+  const Social({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child:Center(
+        child: Text("Social fragment"),
+      ) ,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'package:assignment/Fragment/Favorite.dart';
+import 'package:assignment/Fragment/Home.dart';
+import 'package:assignment/Fragment/Profile.dart';
+import 'package:assignment/Fragment/Search.dart';
+import 'package:assignment/Fragment/Social.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 //Source code Follow
@@ -21,66 +26,44 @@ class MyApp extends StatelessWidget{
 
 class HomeActivity extends StatelessWidget{
 
-  var MyItem=[
-    {"img":"https://cdn.rabbil.com/photos/images/2022/11/04/rabbilVai.png","title":"Maruf Hasan"},
-    {"img":"https://cdn.rabbil.com/photos/images/2022/11/04/rabbilVai.png","title":"Jamal"},
-    {"img":"https://cdn.rabbil.com/photos/images/2022/11/04/rabbilVai.png","title":"Kamal"},
-    {"img":"https://cdn.rabbil.com/photos/images/2022/11/04/rabbilVai.png","title":"Rofiq"},
-    {"img":"https://cdn.rabbil.com/photos/images/2022/11/04/rabbilVai.png","title":"Shofiq"},
-    {"img":"https://cdn.rabbil.com/photos/images/2022/11/04/rabbilVai.png","title":"Rasel"},
-    {"img":"https://cdn.rabbil.com/photos/images/2022/11/04/rabbilVai.png","title":"Rakib"}
-  ];
-  
-  mySnacbar(context,msg){
-    return ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text(msg))
-    );
-  }
+
 
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      //Appbar=============
-      appBar: AppBar(
-        title: Text("My First app"),
-      ),
-
-      body:
-      //ListView
-      GridView.builder(
-          itemCount: MyItem.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 1,
-            childAspectRatio: 1.2
-          ),
-          itemBuilder: (context,index){
-            return GestureDetector(
-              onTap: (){
-                mySnacbar(context, MyItem[index]['title']);
-              },
-              child: Container(
-                margin: EdgeInsets.all(10),
-                width: double.infinity,
-                height:200,
-                child: Image.network(MyItem[index]['img']!,fit: BoxFit.fill,),
-              ),
-            );
-          },
-
-      )
-
-      ,
+   return DefaultTabController(
+       length: 5,
+       child: Scaffold(
+         appBar: AppBar(
+           title: Text('Tab Layout'),
+           centerTitle: true,
+           bottom: TabBar(
+             isScrollable: true,
+             tabs: [
+               Tab(icon: Icon(Icons.home),text: "Home",),
+               Tab(icon: Icon(Icons.search),text: "Search",),
+               Tab(icon: Icon(Icons.person),text: "Profile",),
+               Tab(icon: Icon(Icons.favorite),text: "favorite",),
+               Tab(icon: Icon(Icons.social_distance),text: "Social",),
 
 
+             ],
+           ),
+         ),
+         body: TabBarView(
+           children: [
+             Home(),
+             Search(),
+             Profile(),
+             Favorite(),
+             Social()
 
 
+           ],
+         ),
+       )
 
-
-
-
-    );
+   );
 
 
   }
