c073b33eec66a66ce7f12a1d17fe0869b96c4e5f
Maruf Hasan <marufhasanmitul@gmail.com>
2023-06-16 07:45:55 +0600

    Navigator,Push,pop,Push Replace,push Remove

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,76 @@
-import 'package:assignment/product_list.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
 
-void main(){
+void main() {
   runApp(const myApp());
 }
-class myApp extends StatelessWidget{
+
+class myApp extends StatelessWidget {
   const myApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return HomepageUI();
+  }
+}
+
+class HomepageUI extends State<HomePage> {
+  int countNumber = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Counter App'),
+      ),
+      body: Center(
+        child: Text(
+          countNumber.toString(),
+          style: TextStyle(fontSize: 30),
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () {
+              countNumber = countNumber + 1;
+              setState(() {});
+            },
+            child: Icon(Icons.add),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              countNumber = countNumber - 1;
+              setState(() {});
+            },
+            child: Icon(Icons.remove),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          FloatingActionButton(
+            onPressed: () {
+              countNumber = 0;
+              setState(() {});
+            },
+            child: Icon(Icons.clear),
+          ),
+        ],
+      ),
     );
   }
 }
