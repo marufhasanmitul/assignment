1222b260e8c45cbbe97bd835bcf525dc3e0d3866
Maruf Hasan <marufhasanmitul@gmail.com>
2023-06-27 22:34:20 +0600

    Todo Apps

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,20 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'HomeScreen.dart';
+
 void main(){
-  runApp(myApp());
+  runApp(MyApp());
 }
-class myApp extends StatelessWidget{
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Stack'),
-      ),
-      body:
-      Center(
-        child: SizedBox(
-          width: 300,
-          height: 300,
-          child: Center(
-            child: Stack(
-              children: <Widget>[
-                Container(
-                  width: 300,
-                  height: 300,
-                  color: Colors.red,
-                  padding: EdgeInsets.all(15),
-                  alignment: Alignment.bottomRight,
-                  child:Text(
-                    'One',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ), //Container
-                Positioned(
-                  top: 80,
-                  left: 80,
-                  child: Container(
-                    width: 250,
-                    height: 250,
-                    color: Colors.black,
-                  ),
-                ), //Container
-                Positioned(
-                  top: 20,
-                  left: 20,
-                  child: Container(
-                    height: 200,
-                    width: 200,
-                    color: Colors.purple,
-                  ),
-                ), //Container
-              ], //<Widget>[]
-            ), //Stack
-          ), //Center
-        ), //SizedBox
-      )
+      title: 'To Do App',
+      home: HomeScreen(),
     );
   }
 }
